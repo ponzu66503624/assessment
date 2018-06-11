662373f0c8dc726f52e3c296abfa9f3d53090a0b
ponzu66503624 <stskm18@gmail.com>
2018-06-11 15:58:03 +0900

    #1 に対する対応をしました。

--- assessment.js
+++ assessment.js
@@ -51,7 +51,7 @@
         }
     };
 
-    const answers = const answers = [
+    const answers = const answers = [　//変更しました。
     '{userName}のいいところは声です。{userName}の特徴的な声は皆を惹きつけ、心に残ります。',
     '{userName}のいいところはまなざしです。{userName}に見つめられた人は、気になって仕方がないでしょう。',
     '{userName}のいいところは情熱です。{userName}の情熱に周りの人は感化されます。',
